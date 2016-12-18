ba62516f521069ef8f8939faa6d3f6b0dfb10e49
putridfoetus <putridfoetus@gmail.com>
2016-12-17 23:33:52 -0600

    Add files via upload

---
+++ envvar.sh
@@ -0,0 +1,5 @@
+banner="Nexus 6 NavBar Tuner enabler by putridfoetus";
+apklist="SystemUI.apk";
+apkbak=/data/media/0/APK-Backup;
+backup=1;
+cleanup=1;
---
+++ extracmd.sh
@@ -0,0 +1,1 @@
+rm -rf /data/data/com.android.systemui*/*
